76ed6ca32b341f93b9a389686d3b1c298861d1cc
Matti Yoo <mattiyoo33@gmail.com>
2025-12-14 22:27:45 +0100

    created simulation java package

---
+++ Group_7/src/main/java/com/example/Group_7/controller/SimulationController.java
@@ -0,0 +1,18 @@
+package com.example.Group_7.controller;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class SimulationController {
+    // Constants for grass calculations
+    private static final double AREA_HA = 5600.0;
+    private static final double CONVERSION_RATE_KG_PER_HA = 100.0;
+    private static final double BASE_TEMPERATURE = 12.0;
+    private static final double RAINFALL = 710.37;
+    private static final double TEMP_INCREASE_PER_YEAR = 0.05;
+    private static final int START_YEAR = 2024;
+    private static final int GRASS_START_YEAR = 2015;
+    
+}
